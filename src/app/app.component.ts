ca34f0f46598c6a0343b7e796d6a5ceaf6bfc953
Michael J. Bennett <michael.bennett@rangle.io>
2019-09-30 10:20:25 -0400

    Generated our first component, hello article

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ng-blog-live-code';
+  title: string;
+  constructor() {
+    this.title = 'Angular Training!';
+  }
 }
