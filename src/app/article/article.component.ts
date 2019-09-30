dec467c6571cfba81e04bbf21c0901f8e1171673
Michael J. Bennett <michael.bennett@rangle.io>
2019-09-30 11:01:28 -0400

    Directive and pipe

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -2,11 +2,19 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-article',
-  templateUrl: './article.component.html',
+  // templateUrl: './article.component.html',
+  template: `
+    <h3>Article Title</h3>
+    <p *ngIf="author">{{ author }}</p>
+    <p>{{ articleDate | date }}</p>
+    <div>{{ body }}</div>
+  `,
   styleUrls: ['./article.component.scss']
 })
 export class ArticleComponent implements OnInit {
-
+  author = 'Michael B.';
+  articleDate = new Date(2019, 8, 29, 12);
+  body = 'Lorem Ipsem, etc, etc, etc';
   constructor() { }
 
   ngOnInit() {
