887b1753dfc5415313f0127c40a96fc2018a64de
Michael J. Bennett <michael.bennett@rangle.io>
2019-09-30 12:01:40 -0400

    Adds some fake data for the blogs

--- src/app/blog-roll/blog-roll.component.ts
+++ src/app/blog-roll/blog-roll.component.ts
@@ -15,11 +15,36 @@ import { Component } from '@angular/core';
 })
 export class BlogRollComponent {
   articles = [
-    'New Spaceship',
-    'Why Solar Panels',
-    'How to write Angular',
-    'Components what?',
-    'Front End JS in a Nutshell',
+    {
+      author: 'Michael B.',
+      body: 'Lorem Ipsum Etc....Lorem Ipsum Etc....Lorem Ipsum Etc....Lorem Ipsum Etc....Lorem Ipsum Etc....Lorem Ipsum Etc....Lorem Ipsum Etc....Lorem Ipsum Etc....Lorem Ipsum Etc....Lorem Ipsum Etc....Lorem Ipsum Etc....Lorem Ipsum Etc....Lorem Ipsum Etc....Lorem Ipsum Etc....Lorem Ipsum Etc....Lorem Ipsum Etc....Lorem Ipsum Etc....Lorem Ipsum Etc....Lorem Ipsum Etc....Lorem Ipsum Etc....Lorem Ipsum Etc....Lorem Ipsum Etc....Lorem Ipsum Etc....Lorem Ipsum Etc....Lorem Ipsum Etc....Lorem Ipsum Etc....Lorem Ipsum Etc....Lorem Ipsum Etc....Lorem Ipsum Etc....',
+      date: new Date(2018, 1, 2),
+      title: 'New Spaceship',
+    },
+    {
+      author: 'Laura T.',
+      body: 'Foo bar baz Foo bar baz Foo bar baz Foo bar baz Foo bar baz Foo bar baz Foo bar baz Foo bar baz Foo bar baz Foo bar baz Foo bar baz Foo bar baz Foo bar baz Foo bar baz Foo bar baz Foo bar baz Foo bar baz Foo bar baz Foo bar baz Foo bar baz Foo bar baz Foo bar baz Foo bar baz Foo bar baz Foo bar baz Foo bar baz ',
+      date: new Date(2019, 3, 1),
+      title: 'How to fix a backend',
+    },
+    {
+      author: 'Bill D.',
+      body: 'Thing1, haha, hello Thing1, haha, hello Thing1, haha, hello Thing1, haha, hello Thing1, haha, hello Thing1, haha, hello Thing1, haha, hello Thing1, haha, hello Thing1, haha, hello Thing1, haha, hello Thing1, haha, hello Thing1, haha, hello Thing1, haha, hello Thing1, haha, hello Thing1, haha, hello Thing1, haha, hello Thing1, haha, hello Thing1, haha, hello Thing1, haha, hello Thing1, haha, hello Thing1, haha, hello Thing1, haha, hello Thing1, haha, hello Thing1, haha, hello Thing1, haha, hello Thing1, haha, hello Thing1, haha, hello Thing1, haha, hello Thing1, haha, hello ',
+      date: new Date(2017, 11, 1),
+      title: 'Bill\'s Post',
+    },
+    {
+      author: '',
+      body: 'Body of a blog',
+      date: new Date(2016, 3, 2),
+      title: 'How to write blogs',
+    },
+    {
+      author: 'Jane D.',
+      body: 'Some text and content etc',
+      date: new Date(2019, 3, 12),
+      title: 'Knowledge sharing',
+    },
   ];
 
   onClick(article: string) {
