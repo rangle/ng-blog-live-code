5b086c0dedec7ef30bfabd6f9fe513473c06fdb1
Michael J. Bennett <michael.bennett@rangle.io>
2019-10-01 13:27:11 -0400

    Adds unsubscribe and error handling

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,23 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FetchService, DataArticle } from '../fetch.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
-  selectedArticle: any;
+export class HomeComponent implements OnDestroy, OnInit {
+  didError = false;
+  selectedArticle: DataArticle;
+  subscription: Subscription;
 
   constructor(private fetchService: FetchService) {}
 
+  ngOnDestroy() {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
+
   ngOnInit() {
-    this.fetchService.articles
+    this.subscription = this.fetchService.articles
       .subscribe((articles: DataArticle[]) => {
         if (articles.length) {
          this.selectedArticle = articles[0];
         }
       }, (error: Error) => {
+        this.didError = true;
         console.log('sad path', error);
       });
   }
